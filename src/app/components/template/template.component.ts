dc00d1fa2d642e61e625ccb16a4862a32ae9af4b
Javier Lozano <javier_l_o@hotmail.com>
2018-11-03 18:04:24 -0500

    Se agrega validaciones visuales a los edits

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+  .ng-invalid.ng-touched:not(form) {
+    border: 1px solid red;
+  }
+  `]
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent  {
+
+   parametro: Object = {
+     cantidadMinimaCaracteres: 3
+   };
+
+   usuario: Object = {
+    nombre: null,
+    apellido: null,
+    email: null
+  };
 
   constructor() { }
 
-  ngOnInit() {
+  guardar( forma: NgForm) {
+    console.log('Formulario Posteado');
+    console.log(forma);
+    console.log('Valor Forma', forma.value);
+    console.log('Usuario', this.usuario);
+
+
   }
+
 
 }
