e2b2862c7e51f1a5f422d6e61e33b1e03350fff4
Javier Lozano <javier_l_o@hotmail.com>
2018-11-06 22:21:10 -0500

    Se crea un formulario con objeto interno aprox data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -10,10 +10,22 @@ export class DataComponent  {
 
   forma: FormGroup;
 
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: 'Javi',
+      apellio: 'Lozano'
+    },
+    correo: 'javi@gmail.com'
+  };
+
   constructor() {
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      'nombre':   new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'apellido': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'nombrecompleto': new FormGroup({
+        'nombre':   new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', [Validators.required, Validators.minLength(3)])
+      }),
       'correo':   new FormControl('', [
                                         Validators.required,
                                         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
